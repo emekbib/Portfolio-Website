b9ca5c5dc1273ade3482f1813dbac36943d4e70e
emekbib <emekbib@students.kennesaw.edu>
2023-09-27 19:28:12 -0400

    are these the new changes?

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("fHoOw_0hRFe5jKY0lA0Nkg");
 
         emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
             .then(function (response) {
